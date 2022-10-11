3b95e675010db0886220403dd5cdc744f7c6be22
mitesh virash <miteshkvirash@gmail.com>
2022-10-11 13:00:47 +0530

    updated

--- src/components/careers/Jobopp.js
+++ src/components/careers/Jobopp.js
@@ -14,25 +14,25 @@ import { FaBusinessTime } from "react-icons/fa";
 
 const Jobopp = () => {
   return (
-    <Flex direction="column" w="100%" py="12" align="center">
-      <Heading fontFamily="goudy" fontSize="50px" fontWeight="400" py="6">
+    <Flex direction="column" w="100%" h="100vh" mb="4rem" align="center">
+      <Heading fontFamily="goudy" fontSize="48px" fontWeight="400" py="6">
         JOB OPPORTUNITIES
       </Heading>
-      <Flex w="100%" py="2">
+      <Flex w="100%" py="2" px="12">
         <Flex
           direction="column"
-          w="40%"
+          w="100%"
           h="100%"
           align="center"
           justify="center"
           textAlign="center"
-          px="12"
-          py="16"
+          bgImage="/images/careers/card-bg.png"
+          py="5rem"
         >
-          <Heading fontFamily="avenir" fontSize="40px">
+          <Heading fontFamily="avenir" fontSize="34px" px="12rem">
             FOLLOW US ON LINKEDIN
           </Heading>
-          <Text fontFamily="veralaRound" fontSize="24px" py="6">
+          <Text fontFamily="veralaRound" fontSize="20px" py="6" px="9rem">
             The best way to stay updated on Job Opportunities at Prime Lifespace
             is to follow us on Linkedin.
           </Text>
@@ -41,6 +41,7 @@ const Jobopp = () => {
         </Flex>
         <Grid
           w="100%"
+          h="100%"
           templateRows="repeat(2, 1fr)"
           templateColumns="repeat(2, 1fr)"
           mx="6"
@@ -50,30 +51,32 @@ const Jobopp = () => {
             <Flex
               direction="column"
               align="center"
+              justify="center"
               w="100%"
               bgGradient="linear(to-b, #DFBD69, #B88746)"
-              my="2"
               textAlign="center"
+              color="white"
             >
-              <Heading fontSize="30px" fontFamily="avenir">
+              <Heading fontSize="24px" px="16" fontFamily="avenir">
                 {job.title}
               </Heading>
               <VStack
                 align="flex-start"
                 fontFamily="veralaRound"
-                fontSize="24px"
+                fontSize="20px"
+                py="4"
               >
                 <Flex align="center" justify="flex-start">
-                  <Icon as={MdLocationOn} />
-                  <Text>{job.loc}</Text>
+                  <Icon as={MdLocationOn} w="6" h="6" />
+                  <Text px="1">{job.loc}</Text>
                 </Flex>
                 <Flex align="center" justify="center">
-                  <Icon as={FaBusinessTime} />
-                  <Text>{job.exp}</Text>
+                  <Icon as={FaBusinessTime} w="6" h="6" />
+                  <Text px="1">{job.exp}</Text>
                 </Flex>
                 <Flex align="center" justify="center">
-                  <Icon as={MdBusinessCenter} />
-                  <Text>{job.type}</Text>
+                  <Icon as={MdBusinessCenter} w="6" h="6" />
+                  <Text px="1">{job.type}</Text>
                 </Flex>
               </VStack>
             </Flex>
