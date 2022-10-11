6b614bf26f44b2f7d6ecb4e98d6f97c6f384e552
mitesh virash <miteshkvirash@gmail.com>
2022-10-11 11:29:41 +0530

    updated

--- src/components/careers/firstSec.js
+++ src/components/careers/firstSec.js
@@ -2,8 +2,8 @@ import { Flex, Heading, Image, Text } from "@chakra-ui/react";
 
 const FirstSec = () => {
   return (
-    <Flex w="100%">
-      <Flex w="100%">
+    <Flex w="100%" h="100vh">
+      <Flex w="65%">
         <Image h="100%" w="100%" src="/images/careers/career-stock.png" />
       </Flex>
 
@@ -12,13 +12,13 @@ const FirstSec = () => {
         justify="center"
         direction="column"
         textAlign="center"
-        w="40%"
-        px="8"
+        w="35%"
+        px="16"
       >
         <Heading fontSize="50px" fontFamily="avenir">
           JOIN OUR TRIBE
         </Heading>
-        <Text fontSize="24px" fontFamily="veralaRound">
+        <Text fontSize="20px" fontFamily="veralaRound">
           Let’s join our efforts towards building the unshakable foundation for
           a better tomorrow.
         </Text>
