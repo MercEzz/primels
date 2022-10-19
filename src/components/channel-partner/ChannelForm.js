49fa600f431ed6fd43546151f4fbb1bfd59d9bad
mitesh virash <miteshkvirash@gmail.com>
2022-10-19 16:46:56 +0530

    updated

--- src/components/channel-partner/ChannelForm.js
+++ src/components/channel-partner/ChannelForm.js
@@ -29,6 +29,36 @@ const ChannelForm = () => {
         px="100px"
         bgImage='linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5)), url("/images/bg-box.jpg")'
       >
+        <Heading
+          initial={{}}
+          fontSize="40px"
+          fontFamily="avenir"
+          lineHeight="57px"
+          textTransform="uppercase"
+          textAlign="center"
+          mt="50px"
+          mb="25px"
+        >
+          where opportunities pave the way for <br />
+          incredible rewards
+        </Heading>
+        <Text
+          px="100px"
+          fontFamily="veralaRound"
+          fontSize="16px"
+          lineHeight="24px"
+          mb="50px"
+        >
+          Your association with us is extremely valuable to us. We support our
+          Channel Partners to catalyst their growth with ours. Join our network
+          and scale to greater heights as we bring to you various reward wining
+          sources from our projects. So come, to be introduced to an achiever's
+          destination reserved for those who make success happen.
+          <Text pt="15px">
+            Please fill in the details below for all your marketing
+            requirements.
+          </Text>
+        </Text>
         <FormControl align="center">
           <Flex w="100%">
             {/* top - left */}
--- src/components/channel-partner/MainSec.js
+++ src/components/channel-partner/MainSec.js
@@ -6,37 +6,29 @@ const MtnHead = motion(Heading);
 
 const MainSec = () => {
   return (
-    <Flex w="100%" h="100%" direction="column" align="center">
-      <Image w="100%" h="100%" src="/images/channel-partner/HANDSHAKE.gif" />
-      <MtnHead
-        initial={{}}
-        fontSize="40px"
-        fontFamily="avenir"
-        lineHeight="57px"
-        textTransform="uppercase"
-        textAlign="center"
-        mt="50px"
-        mb="25px"
+    <Flex
+      w="100%"
+      h="568px"
+      direction="column"
+      align="center"
+      pos="relative"
+      overflow="hidden"
+    >
+      <video
+        autoPlay
+        muted
+        loop
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          minHeight: "100%",
+          minWidth: "100%",
+          // zIndex: -1,
+        }}
       >
-        where opportunities pave the way for <br />
-        incredible rewards
-      </MtnHead>
-      <Text
-        px="100px"
-        fontFamily="veralaRound"
-        fontSize="16px"
-        lineHeight="24px"
-        mb="25px"
-      >
-        Your association with us is extremely valuable to us. We support our
-        Channel Partners to catalyst their growth with ours. Join our network
-        and scale to greater heights as we bring to you various reward wining
-        sources from our projects. So come, to be introduced to an achiever's
-        destination reserved for those who make success happen.
-        <Text pt="15px">
-          Please fill in the details below for all your marketing requirements.
-        </Text>
-      </Text>
+        <source src="/images/channel-partner/HANDSHAKE2.mp4" type="video/mp4" />
+      </video>
     </Flex>
   );
 };
