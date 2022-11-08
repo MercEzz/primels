324eac1b19cc3975e0a5e0db9d20b76390896a8a
mitesh virash <miteshkvirash@gmail.com>
2022-11-08 19:03:00 +0530

    updated

--- src/components/arete-homes/EleventhSec.js
+++ src/components/arete-homes/EleventhSec.js
@@ -1,7 +1,411 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React from "react";
+import {
+  Flex,
+  Heading,
+  Text,
+  Button,
+  Divider,
+  Box,
+  UnorderedList,
+  ListItem,
+} from "@chakra-ui/react";
+import { ImLocation } from "react-icons/im";
 
-const EleventhSec = () => {
-  return <div>EleventhSec</div>;
+const NinethSec = () => {
+  return (
+    <Flex
+      w="100%"
+      h="100%"
+      px="100px"
+      pt="50px"
+      pb="50px"
+      direction="column"
+      align="center"
+      justify="center"
+    >
+      <Text
+        fontFamily="goudy"
+        fontSize="40px"
+        lineHeight="48px"
+        textTransform="uppercase"
+      >
+        LOCATION ADVANTAGE
+      </Text>
+      <Heading
+        fontFamily="avenir"
+        fontSize="34px"
+        lineHeight="48px"
+        pt="25px"
+        pb="50px"
+      >
+        PONNERI - A SMART CITY BY JICA
+      </Heading>
+      <Flex w="100%" h="100%" align="flex-start" justify="flex-start">
+        <Flex
+          direction="column"
+          w="100%"
+          h="100%"
+          align="center"
+          justify="center"
+        >
+          <Flex w="100%" h="100%" mb="15px">
+            <iframe
+              class="gmap_iframe"
+              width="662"
+              height="458"
+              frameborder="0"
+              scrolling="no"
+              marginheight="0"
+              marginwidth="0"
+              src="https://maps.google.com/maps?width=918&amp;height=700&amp;hl=en&amp;q=Prime lifespace&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+            ></iframe>
+          </Flex>
+          <Flex w="100%" h="100%" mb="20px">
+            icons
+          </Flex>
+          <Flex w="100%" h="100%">
+            <Button
+              w="100%"
+              h="51px"
+              fontSize="16px"
+              fontFamily="avenir"
+              fontWeight="bold"
+              bgGradient="linear(to-b, #B88746 ,#DFBD69)"
+              color="white"
+              _hover={{
+                bgGradient: "linear(to-b, #DFBD69, #B88746)",
+              }}
+              _active={{ bgGradient: "linear(to-b, #B88746 ,#DFBD69)" }}
+            >
+              <ImLocation size="30px" /> SHOW LOCATION ON GOOGLE MAPS
+            </Button>
+          </Flex>
+        </Flex>
+        <Flex w="100%" h="100%" align="center" direction="column">
+          <Flex
+            w="100%"
+            h="100%"
+            alignSelf="flex-start"
+            justify="space-between"
+          >
+            <Flex
+              h="100%"
+              direction="column"
+              justify="flex-start"
+              align="center"
+              px="25px"
+            >
+              <Divider
+                h="50px"
+                orientation="vertical"
+                border="3px solid"
+                borderColor="#DFBD69"
+                bgColor="#DFBD69"
+              />
+              <Box
+                height="30px"
+                width="30px"
+                backgroundColor="#DFBD69"
+                border="3px solid white"
+                boxShadow="0px 0px 0px 3px #B88746"
+                borderRadius="50%"
+                my="1"
+              />
+              <Divider
+                h="50px"
+                orientation="vertical"
+                border="3px solid"
+                borderColor="#DFBD69"
+                bgColor="#DFBD69"
+              />
+            </Flex>
+
+            <Heading
+              alignSelf="center"
+              fontFamily="avenir"
+              fontSize="20px"
+              mr="25px"
+            >
+              5 Mins
+            </Heading>
+            <UnorderedList
+              alignSelf="center"
+              textTransform="uppercase"
+              fontFamily="veralaRound"
+              fontSize="16px"
+            >
+              <ListItem pb="10px">
+                Walk to Velammal International School{" "}
+              </ListItem>
+              <ListItem pb="10px">DRIVE TO Origin by Mahindra </ListItem>
+              <ListItem pb="10px">DRIVE TO Ponneri Railway Station</ListItem>
+            </UnorderedList>
+          </Flex>
+          <Flex
+            w="100%"
+            h="100%"
+            alignSelf="flex-start"
+            justify="space-between"
+          >
+            {" "}
+            <Flex
+              h="100%"
+              direction="column"
+              justify="flex-start"
+              align="center"
+              px="25px"
+            >
+              <Divider
+                h="50px"
+                orientation="vertical"
+                border="3px solid"
+                borderColor="#DFBD69"
+                bgColor="#DFBD69"
+              />
+              <Box
+                height="30px"
+                width="30px"
+                backgroundColor="#DFBD69"
+                border="3px solid white"
+                boxShadow="0px 0px 0px 3px #B88746"
+                borderRadius="50%"
+                my="1"
+              />
+              <Divider
+                h="50px"
+                orientation="vertical"
+                border="3px solid"
+                borderColor="#DFBD69"
+                bgColor="#DFBD69"
+              />
+            </Flex>
+            <Heading
+              w="fit-content"
+              display="flex"
+              alignSelf="center"
+              fontFamily="avenir"
+              fontSize="20px"
+              mr="25px"
+            >
+              10 Mins
+            </Heading>
+            <UnorderedList
+              textTransform="uppercase"
+              alignSelf="center"
+              fontFamily="veralaRound"
+              fontSize="16px"
+            >
+              <ListItem pb="10px">CONNECT TO Outer Ring Road</ListItem>
+              <ListItem pb="10px">DRIVE TO Siruvapuri Murugan Temple </ListItem>
+              <ListItem pb="10px">VELS Medical Hospital</ListItem>
+            </UnorderedList>
+          </Flex>
+          <Flex h="100%" alignSelf="flex-start" justify="center">
+            {" "}
+            <Flex
+              h="100%"
+              direction="column"
+              justify="flex-start"
+              align="center"
+              px="25px"
+            >
+              <Divider
+                h="50px"
+                orientation="vertical"
+                border="3px solid"
+                borderColor="#DFBD69"
+                bgColor="#DFBD69"
+              />
+              <Box
+                height="30px"
+                width="30px"
+                backgroundColor="#DFBD69"
+                border="3px solid white"
+                boxShadow="0px 0px 0px 3px #B88746"
+                borderRadius="50%"
+                my="1"
+              />
+              <Divider
+                h="50px"
+                orientation="vertical"
+                border="3px solid"
+                borderColor="#DFBD69"
+                bgColor="#DFBD69"
+              />
+            </Flex>
+            <Heading
+              alignSelf="center"
+              fontFamily="avenir"
+              fontSize="20px"
+              mr="25px"
+            >
+              20 Mins
+            </Heading>
+            <Flex alignSelf="center">
+              <UnorderedList
+                textTransform="uppercase"
+                fontFamily="veralaRound"
+                fontSize="16px"
+              >
+                <ListItem pb="10px">DRIVE TO REDHILLS</ListItem>
+                <ListItem pb="10px">PULICAT LAKE </ListItem>
+                <ListItem pb="10px">DRIVE TO GUMMIDIPOONDI SIPCOT</ListItem>
+              </UnorderedList>
+            </Flex>
+          </Flex>
+          <Flex h="100%" alignSelf="flex-start" justify="center">
+            {" "}
+            <Flex
+              h="100%"
+              direction="column"
+              justify="flex-start"
+              align="center"
+              px="25px"
+            >
+              <Divider
+                h="50px"
+                orientation="vertical"
+                border="3px solid"
+                borderColor="#DFBD69"
+                bgColor="#DFBD69"
+              />
+              <Box
+                height="30px"
+                width="30px"
+                backgroundColor="#DFBD69"
+                border="3px solid white"
+                boxShadow="0px 0px 0px 3px #B88746"
+                borderRadius="50%"
+                my="1"
+              />
+              <Divider
+                h="50px"
+                orientation="vertical"
+                border="3px solid"
+                borderColor="#DFBD69"
+                bgColor="#DFBD69"
+              />
+            </Flex>
+            <Heading
+              alignSelf="center"
+              fontFamily="avenir"
+              fontSize="20px"
+              mr="25px"
+            >
+              30 Mins
+            </Heading>
+            <Flex alignSelf="center">
+              <UnorderedList
+                textTransform="uppercase"
+                fontFamily="veralaRound"
+                fontSize="16px"
+              >
+                <ListItem pb="10px">DRIVE TO SRI CITY</ListItem>
+                <ListItem pb="10px">DRIVE TO ADANI PORT</ListItem>
+                <ListItem pb="10px">DRIVE TO Ponneri Railway Station</ListItem>
+              </UnorderedList>
+            </Flex>
+          </Flex>
+          <Flex h="100%" alignSelf="flex-start" justify="center">
+            {" "}
+            <Flex
+              h="100%"
+              direction="column"
+              justify="flex-start"
+              align="center"
+              px="25px"
+            >
+              <Divider
+                h="50px"
+                orientation="vertical"
+                border="3px solid"
+                borderColor="#DFBD69"
+                bgColor="#DFBD69"
+              />
+              <Box
+                height="30px"
+                width="30px"
+                backgroundColor="#DFBD69"
+                border="3px solid white"
+                boxShadow="0px 0px 0px 3px #B88746"
+                borderRadius="50%"
+                my="1"
+              />
+              <Divider
+                h="50px"
+                orientation="vertical"
+                border="3px solid"
+                borderColor="#DFBD69"
+                bgColor="#DFBD69"
+              />
+            </Flex>
+            <Heading
+              alignSelf="center"
+              fontFamily="avenir"
+              fontSize="20px"
+              mr="25px"
+            >
+              40 Mins
+            </Heading>
+            <Flex alignSelf="center">
+              <UnorderedList
+                textTransform="uppercase"
+                fontFamily="veralaRound"
+                fontSize="16px"
+              >
+                <ListItem pb="10px">DRIVE TO CHENNAI AIRPORT</ListItem>
+              </UnorderedList>
+            </Flex>
+          </Flex>
+          <Flex h="100%" alignSelf="flex-start" justify="center">
+            {" "}
+            <Flex
+              h="100%"
+              direction="column"
+              justify="flex-start"
+              align="center"
+              px="25px"
+            >
+              <Divider
+                h="50px"
+                orientation="vertical"
+                border="3px solid"
+                borderColor="#DFBD69"
+                bgColor="#DFBD69"
+              />
+              <Box
+                height="30px"
+                width="30px"
+                backgroundColor="#DFBD69"
+                border="3px solid white"
+                boxShadow="0px 0px 0px 3px #B88746"
+                borderRadius="50%"
+                my="1"
+              />
+            </Flex>
+            <Heading
+              alignSelf="center"
+              fontFamily="avenir"
+              fontSize="20px"
+              mr="25px"
+            >
+              2 Hours
+            </Heading>
+            <Flex alignSelf="center">
+              <UnorderedList
+                textTransform="uppercase"
+                fontFamily="veralaRound"
+                fontSize="16px"
+              >
+                <ListItem pb="10px">DRIVE TO TIRUPATI</ListItem>
+              </UnorderedList>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 };
 
-export default EleventhSec;
+export default NinethSec;
--- src/components/arete-homes/TwelvethSec.js
+++ src/components/arete-homes/TwelvethSec.js
@@ -1,7 +1,274 @@
+import {
+  Flex,
+  Heading,
+  Image,
+  Text,
+  Divider,
+  Box,
+  Button,
+  TableContainer,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+} from "@chakra-ui/react";
 import React from "react";
 
-const TwelvethSec = () => {
-  return <div>TwelvethSec</div>;
+const TenthSec = () => {
+  return (
+    <Flex
+      w="100%"
+      h="100%"
+      my="50px"
+      px="100px"
+      align="center"
+      justify="center"
+      direction="column"
+    >
+      <Text fontFamily="goudy" fontSize="40px" mb="25px">
+        SMART INVEST
+      </Text>
+      <Heading fontFamily="avenir" fontSize="34px" textAlign="center" mb="50px">
+        RETURNS HIGHERN THAN YOUR IMAGINATION
+      </Heading>
+      <Flex w="100%" h="100%">
+        <Flex w="100%" h="100%">
+          <Image
+            // pos="absolute"
+            h="568px"
+            w="707px"
+            objectPosition="center top"
+            objectFit="cover"
+            src="/images/ARETE HOMES/SMART INVEST.jpg"
+          />
+        </Flex>
+        <Flex w="100%" h="100%">
+          <Flex
+            h="100%"
+            direction="column"
+            justify="flex-start"
+            align="center"
+            px="25px"
+          >
+            <Divider
+              h="80px"
+              orientation="vertical"
+              border="3px solid"
+              borderColor="#DFBD69"
+              bgColor="#DFBD69"
+            />
+            <Box
+              height="30px"
+              width="30px"
+              backgroundColor="#DFBD69"
+              border="3px solid white"
+              boxShadow="0px 0px 0px 3px #B88746"
+              borderRadius="50%"
+              my="1"
+            />
+          </Flex>
+          <Flex w="100%" direction="column" align="center" justify="center">
+            <Heading
+              w="100%"
+              fontSize="24px"
+              textAlign="left"
+              fontFamily="avenir"
+              mb="25px"
+            >
+              EARN HIGHEST RENTAL INCOME (ROI)
+            </Heading>
+            <Flex mb="25px" alignSelf="flex-start">
+              <TableContainer>
+                <Table>
+                  <Thead
+                    textTransform="uppercase"
+                    bgGradient="linear(to-b, #DFBD69, #B88746)"
+                  >
+                    {" "}
+                    <Tr>
+                      <Th
+                        fontFamily="veralaRound"
+                        fontSize="20px"
+                        fontWeight="400"
+                        color="white"
+                        textAlign="center"
+                        border="1px solid #B88746"
+                        p="15px"
+                      >
+                        SMART HOME
+                      </Th>
+                      <Th
+                        fontFamily="veralaRound"
+                        fontSize="20px"
+                        fontWeight="400"
+                        color="white"
+                        textAlign="center"
+                        border="1px solid #B88746"
+                        p="15px"
+                      >
+                        PRICE
+                      </Th>
+                      <Th
+                        fontFamily="veralaRound"
+                        fontSize="20px"
+                        fontWeight="400"
+                        color="white"
+                        textAlign="center"
+                        border="1px solid #B88746"
+                        p="15px"
+                      >
+                        RENTAL INCOME
+                      </Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    <Tr>
+                      <Td
+                        w="100%"
+                        textAlign="left"
+                        fontFamily="veralaRound"
+                        fontSize="18px"
+                        border="1px solid #B88746"
+                        p="25px"
+                      >
+                        <Text
+                          as="span"
+                          fontWeight="bold"
+                          fontFamily="avenir"
+                          fontSize="24px"
+                        >
+                          2 BHK
+                        </Text>{" "}
+                        <br />
+                        1085 SQFT.
+                      </Td>
+                      <Td
+                        w="100%"
+                        textAlign="left"
+                        fontFamily="veralaRound"
+                        fontSize="18px"
+                        border="1px solid #B88746"
+                        p="25px"
+                      >
+                        <Text
+                          as="span"
+                          fontWeight="bold"
+                          fontFamily="avenir"
+                          fontSize="24px"
+                        >
+                          Rs. 52 LAKHS
+                        </Text>{" "}
+                        <br />
+                        Fully Furnished With <br />
+                        White Goods
+                      </Td>
+                      <Td
+                        w="100%"
+                        textAlign="left"
+                        fontFamily="veralaRound"
+                        fontSize="18px"
+                        border="1px solid #B88746"
+                        p="25px"
+                      >
+                        <Text
+                          as="span"
+                          fontWeight="bold"
+                          fontFamily="avenir"
+                          fontSize="24px"
+                        >
+                          Rs. 32,000
+                        </Text>{" "}
+                        <br />
+                        Per Month
+                      </Td>
+                    </Tr>
+                    <Tr>
+                      <Td
+                        w="100%"
+                        textAlign="left"
+                        fontFamily="veralaRound"
+                        fontSize="18px"
+                        border="1px solid #B88746"
+                        p="25px"
+                      >
+                        <Text
+                          as="span"
+                          fontWeight="bold"
+                          fontFamily="avenir"
+                          fontSize="24px"
+                        >
+                          3 BHK
+                        </Text>{" "}
+                        <br />
+                        1787 SQFT.
+                      </Td>
+                      <Td
+                        w="100%"
+                        textAlign="left"
+                        fontFamily="veralaRound"
+                        fontSize="18px"
+                        border="1px solid #B88746"
+                        p="25px"
+                      >
+                        <Text
+                          as="span"
+                          fontWeight="bold"
+                          fontFamily="avenir"
+                          fontSize="24px"
+                        >
+                          Rs. 85 LAKHS
+                        </Text>{" "}
+                        <br />
+                        Fully Furnished With <br /> White Goods
+                      </Td>
+                      <Td
+                        w="100%"
+                        textAlign="left"
+                        fontFamily="veralaRound"
+                        fontSize="18px"
+                        border="1px solid #B88746"
+                        p="25px"
+                      >
+                        <Text
+                          as="span"
+                          fontWeight="bold"
+                          fontFamily="avenir"
+                          fontSize="24px"
+                        >
+                          Rs. 48,000
+                        </Text>{" "}
+                        <br />
+                        Per Month
+                      </Td>
+                    </Tr>
+                  </Tbody>
+                </Table>
+              </TableContainer>
+            </Flex>
+            <Heading fontFamily="avenir" fontSize="20px" mb="25px">
+              ASSURED RENTAL INCOME FOR 3 YEARS!
+            </Heading>
+            <Button
+              w="146px"
+              h="51px"
+              fontSize="16px"
+              fontFamily="avenir"
+              bgGradient="linear(to-b, #B88746 ,#DFBD69)"
+              color="white"
+              _hover={{
+                bgGradient: "linear(to-b, #DFBD69, #B88746)",
+              }}
+              _active={{ bgGradient: "linear(to-b, #B88746 ,#DFBD69)" }}
+            >
+              KNOW MORE
+            </Button>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 };
 
-export default TwelvethSec;
+export default TenthSec;
