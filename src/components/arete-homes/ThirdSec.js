c57d0d85e21c2e4e5dc41dcc05758165b1bc94ca
mitesh virash <miteshkvirash@gmail.com>
2022-11-10 16:25:12 +0530

    updated

--- src/components/arete-homes/ThirdSec.js
+++ src/components/arete-homes/ThirdSec.js
@@ -1,17 +1,60 @@
 import { Flex, Text, Image } from "@chakra-ui/react";
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { motion, useAnimation, useInView } from "framer-motion";
+
+const MtnTxt = motion(Text);
+const MtnFlx = motion(Flex);
 
 const ThirdSec = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, {
+    once: true,
+    margin: "0px 100px -50px 0px",
+  });
+  const animateY = useAnimation();
+  const animateScale = useAnimation();
+
+  useEffect(() => {
+    if (useInView) {
+      animateY.start({
+        y: 0,
+        opacity: 1,
+      });
+      animateScale.start({
+        scale: 1,
+      });
+    }
+  }, [isInView, animateY, animateScale]);
+
   return (
     <Flex w="100%" h="100%" direction="column" align="center" justify="center">
-      <Text fontFamily="goudy" fontSize="40px" mb="50px">
+      <MtnTxt
+        initial={{ y: -50, opacity: 0 }}
+        animate={animateY}
+        transition={{ duration: 2, type: "just" }}
+        fontFamily="goudy"
+        fontSize="40px"
+        mb="50px"
+      >
         MASTER PLAN
-      </Text>
+      </MtnTxt>
       <Flex w="100%" h="100%">
-        <Flex w="100%" h="100%">
-          <Image h="500px" src="/images/ARETE HOMES/MASTER PLAN.jpg" />
-        </Flex>
-        <Flex></Flex>
+        <MtnFlx
+          initial={{ scale: 0.6 }}
+          animate={animateScale}
+          transition={{ duration: 2, type: "just" }}
+          w="100%"
+          h="100%"
+        >
+          <Image h="500px" src="/images/arete-homes/MASTER PLAN.jpg" />
+        </MtnFlx>
+        <MtnFlx
+          initial={{ scale: 0.6 }}
+          animate={animateScale}
+          transition={{ duration: 2, type: "just" }}
+          w="100%"
+          h="100%"
+        ></MtnFlx>
       </Flex>
     </Flex>
   );
