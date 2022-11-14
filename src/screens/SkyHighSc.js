1095b9f56fa4d65921e7f74278d8c48fd55bba01
mitesh virash <miteshkvirash@gmail.com>
2022-11-14 12:53:45 +0530

    updated

--- src/screens/SkyHighSc.js
+++ src/screens/SkyHighSc.js
@@ -1,17 +1,17 @@
 import React from "react";
 import DownloadBtn from "../components/sky-high/DownloadBtn";
 import DropDown from "../components/sky-high/DropDown";
-import EightSec from "../components/sky-high/EightSec";
-import FifthSec from "../components/sky-high/FifthSec";
-import FirstSec from "../components/sky-high/FirstSec";
-import FourthSec from "../components/sky-high/FourthSec";
-import LastSec from "../components/sky-high/LastSec";
-import NinethSec from "../components/sky-high/NinethSec";
-import SecondSec from "../components/sky-high/SecondSec";
-import SeventhSec from "../components/sky-high/SeventhSec";
-import SixthSec from "../components/sky-high/SixthSec";
-import TenthSec from "../components/sky-high/TenthSec";
-import ThirdSec from "../components/sky-high/ThirdSec";
+import EightSec from "../components/sky-high/DelightfullMix";
+import FifthSec from "../components/sky-high/LiveLargest";
+import FirstSec from "../components/sky-high/HeroSec";
+import FourthSec from "../components/sky-high/CraftedLiving";
+import LastSec from "../components/sky-high/HappyFamilies";
+import NinethSec from "../components/sky-high/LocationAdv";
+import SecondSec from "../components/sky-high/AnIconic";
+import SeventhSec from "../components/sky-high/AwakenBanner";
+import SixthSec from "../components/sky-high/PonneriGymClub";
+import TenthSec from "../components/sky-high/SmartInvest";
+import ThirdSec from "../components/sky-high/ExperienceBanner";
 
 const SkyHighSc = () => {
   return (
