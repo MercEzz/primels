4bed3344296edc872f88079fae2994938e8be86a
Alroy Fernandes <alroy.fern7@gmail.com>
2022-12-07 16:30:15 +0530

    prime talks automated

--- src/components/news-media/PrimeTalks.js
+++ src/components/news-media/PrimeTalks.js
@@ -1,11 +1,39 @@
 import { Flex, Text, Divider, Box } from "@chakra-ui/react";
 import { useInView } from "framer-motion";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRef } from "react";
-
+import "./nm.css"
 const FourthSec = () => {
+  const videos=[{source:"https://www.youtube.com/embed/Psmw1t_S6-c"},{source:"https://www.youtube.com/embed/Psmw1t_S6-c"},{source:"https://www.youtube.com/embed/Psmw1t_S6-c"},{source:"https://www.youtube.com/embed/Psmw1t_S6-c"},{source:"https://www.youtube.com/embed/Psmw1t_S6-c"}]
+  const [vid,setVid] = useState({id:"0",src:videos[0].source});
+  const changeVid=(e)=>{
+    //console.log(e.ta);
+    document.getElementById(`${vid.id}`).classList.remove("bold");
+    const id = e.target.id;
+    document.getElementById(`${id}`).classList.add("bold");
+    setVid({id:String(id),src:videos[id].source});
+  }
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
+  useEffect(() => {
+    if(isInView===true){
+      setTimeout(() => {
+        document.getElementById("0").click();
+      }, 2000);
+      setTimeout(() => {
+        document.getElementById("1").click();
+      }, 4000);
+      setTimeout(() => {
+        document.getElementById("2").click();
+      }, 6000);
+      setTimeout(() => {
+        document.getElementById("3").click();
+      }, 8000);
+      setTimeout(() => {
+        document.getElementById("4").click();
+      }, 10000);
+    }
+},[isInView]);
   return (
     <Flex
       ref={ref}
@@ -39,7 +67,7 @@ const FourthSec = () => {
           <embed
             height="100%"
             width="100%"
-            src="https://www.youtube.com/embed/Psmw1t_S6-c"
+            src={vid.src}
             title="The Economic Times | Best Realty Brands 2021 - Prime LifeSpace Developers"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -57,7 +85,7 @@ const FourthSec = () => {
           align="center"
         >
           <Divider
-            h="1.562rem"
+            h={`${((2 * vid.id) * 1.570) + 1.570}rem`}
             orientation="vertical"
             border="3px solid"
             borderColor="#DFBD69"
@@ -84,20 +112,20 @@ const FourthSec = () => {
             fontSize="1.25rem"
             justify="center"
           >
-            <Text pb="1.562rem" mt="" pl="1.562rem">
+            <Text _hover={{cursor:"pointer"}} id={0} onClick={(e)=>changeVid(e)} pb="1.562rem" mt="2rem" pl="1.562rem" verticalAlign="middle">
               Best Realty Brand 2021 The Economic Times
             </Text>
-            <Text pb="1.562rem" pl="1.562rem">
+            <Text _hover={{cursor:"pointer"}} id={1} onClick={(e)=>changeVid(e)} pb="1.562rem" pl="1.562rem">
               The Future Of Real Estate
             </Text>
-            <Text pb="1.562rem" pl="1.562rem">
+            <Text _hover={{cursor:"pointer"}} id={2} onClick={(e)=>changeVid(e)} pb="1.562rem" pl="1.562rem" verticalAlign="middle">
               Best Real Estate Investment 8% Guaranteed Rental Return At Arete
               Homes
             </Text>
-            <Text pb="1.562rem" pl="1.562rem">
+            <Text _hover={{cursor:"pointer"}} id={3} onClick={(e)=>changeVid(e)} pb="1.562rem" pl="1.562rem" verticalAlign="middle">
               Sky High Tower Launch Event
             </Text>
-            <Text pl="1.562rem">
+            <Text _hover={{cursor:"pointer"}} id={4} onClick={(e)=>changeVid(e)} pl="1.562rem" verticalAlign="middle">
               Dr. Janani Rex Talks About Arete Homes Sky High Towers
             </Text>
           </Flex>
